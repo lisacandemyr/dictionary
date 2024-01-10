6cd0f43ca68cb0eaf36a07c12e43de82eac210d1
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2024-01-10 11:29:17 +0000

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Lisa</small>
+        </footer>
+      </div>
     </div>
   );
 }
