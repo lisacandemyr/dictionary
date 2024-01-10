23f0fd096ae0db565cdd8507fa05c118338dd169
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2024-01-10 12:02:14 +0000

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
-  function search(event) {
-    event.preventDefault();
-    alert(`Searching for ${keyword}`);
-  }
-
   function handleKeywordChange(event) {
     event.preventDefault();
     setKeyword(event.target.value);
+  }
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiKey = "344027118o9t0bba5e252d5b7747161f";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
